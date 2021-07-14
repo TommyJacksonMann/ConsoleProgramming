f7edada9a77cc47dc402fda6d2974d4b82fc1550
tommy <thomas.mann@mymail.champlain.edu>
2021-07-14 14:08:33 -0400

    Added NavMesh Stuff

---
+++ CurveEditor/Assets/Scripts/NavMesh/NavMesh.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NavMesh
+{
+    [SerializeField, HideInInspector]
+    public List<Vector3> points;
+
+    float verticalOffsetFromGround = .1f;
+
+    public NavMesh(Vector3 center)
+    {
+        RaycastHit hit;
+        Vector3 offset = Vector3.zero;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(center, Vector3.down, out hit, Mathf.Infinity))
+        {
+            offset.y = -hit.distance + verticalOffsetFromGround;
+        }
+        
+        points = new List<Vector3>
+        {
+            offset + Vector3.left,
+            offset + Vector3.forward,
+            offset + Vector3.right
+        };
+    }
+    public int NumPoints
+    {
+        get
+        {
+            return points.Count;
+        }
+    }
+    public int NumFaces
+    {
+        get
+        {
+            return points.Count - 2;
+        }
+    }
+
+    public void InsertPoint(int index, Vector3 point)
+    {
+        points.Insert(index, point);
+    }
+
+    public void MovePoint(int index, Vector3 position)
+    {
+
+    }
+}
---
+++ CurveEditor/Assets/Scripts/NavMesh/NavMeshEditor.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(NavMeshCreator))]
+public class NavMeshEditor : Editor
+{
+    NavMeshCreator creator;
+
+    NavMesh NavMesh
+    {
+        get
+        {
+            return creator.navMesh;
+        }
+    }
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUI.BeginChangeCheck();
+        if (GUILayout.Button("Create New"))
+        {
+            Undo.RecordObject(creator, "Create New");
+            creator.CreateNavMesh();
+            SceneView.RepaintAll();
+        }
+    }
+
+    private void OnSceneGUI()
+    {
+        if (Event.current.type == EventType.Repaint)
+        {
+            creator.UpdateMesh(NavMesh.points);
+        }
+        Draw();
+        Input();
+    }
+
+    private void OnEnable()
+    {
+        creator = (NavMeshCreator)target;
+    }
+
+    void Draw()
+    {
+        for (int i = 0; i < creator.navMesh.points.Count; i++)
+        {
+            Handles.color = creator.PointColor;
+            float handleSize = creator.PointSize;
+            Vector3 newPos = Handles.FreeMoveHandle(creator.navMesh.points[i], Quaternion.identity, handleSize, Vector3.zero, Handles.CylinderHandleCap);
+            if (creator.navMesh.points[i] != newPos)
+            {
+                Undo.RecordObject(creator, "Move point");
+                creator.navMesh.MovePoint(i, newPos);
+            }
+        }
+    }
+    void Input()
+    {
+        Event guiEvent = Event.current;
+        Vector3 mousePos = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin;
+    }
+}
